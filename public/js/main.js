6009e6fb692181dae807471ef0559202202b0434
Deon Hua <deonhbci@gmail.com>
2015-05-09 00:23:12 -0400

    router functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,25 @@
+// File is referenced in index.html and renders on all pages.
+
+// Declare application
 var app = angular.module('myApp', ['ui.router', 'parse-angular']);
 
+// UI-Router
 app.config(function ($stateProvider, $urlRouterProvider){
-    
+
     // Redirect to home
-    $urlRouterProvider.otherwise("/"); 
+    $urlRouterProvider.otherwise("/");
+    
+    // Define states
+    $stateProvider
+    // Root
+    .state("home", {
+        url: "/",
+        templateUrl: "/partials/home.html"
+    })
+    // Reservations view
+    .state('reservations', {
+        url: "/reservations",
+        templateUrl: "/partials/reservations.html",
+        controller: "ReservationsController"
+    });
 });
---
+++ public/js/reservationsController.js
@@ -0,0 +1,4 @@
+app.controller("ReservationsController", ['$scope', 
+function($scope){
+    
+}]);
