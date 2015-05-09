3b8382aa555388d4504a644069a982cb91c84f0e
Deon Hua <deonhbci@gmail.com>
2015-05-08 22:35:34 -0400

    dependencies

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        watch: {
+            sass:{
+                files: ['scss/style.scss'],
+                tasks: ['sass'],
+                options:{
+                    livereload: true
+                }
+            },
+            css:{
+                files: ['css/style.css'],
+                tasks: ['cssmin'],
+                options: {
+                    livereload: true
+                }
+            }
+
+        },
+        sass: {
+            options:{
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'css/style.css': 'scss/style.scss'
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'css/style.min.css': 'css/style.css'
+                }
+            }
+        },
+        'http-server': {
+            'dev': {
+                root: '.',
+                port: 8000,
+                host: "0.0.0.0",
+                showDir : true,
+                autoIndex: true,
+                ext: "html",
+                runInBackground: true
+            }
+
+        }
+    });
+
+    //grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-http-server');
+    grunt.registerTask('default', ['http-server','watch']);
+    grunt.registerTask('minify', ['cssmin']);
+
+};
